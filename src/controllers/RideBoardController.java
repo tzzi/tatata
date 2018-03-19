a51420fd1a17aefd052a2703e8b157bfb59506b4
1104-23 <1104-23@1104-23-PC>
2018-03-19 10:12:54 +0900

    예성 놀이기구 게시판 컨트롤러 연동

--- src/controllers/RideBoardController.java
+++ src/controllers/RideBoardController.java
@@ -1,5 +1,16 @@
 package controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/rideboard")
 public class RideBoardController {
 
+	@RequestMapping("/view.do")
+	public String rideHandle() {
+		
+		return "view";
+
+	}
 }
