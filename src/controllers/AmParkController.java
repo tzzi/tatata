852f789ac4e90e9a6b4a4bee717a1fa03c0aef81
1104-23 <1104-23@1104-23-PC>
2018-03-20 13:03:01 +0900

    클래스 이름변경

--- src/controllers/AmParkController.java
+++ src/controllers/AmParkController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/rideboard")
-public class RideBoardController {
+public class AmParkController {
 
 	@RequestMapping("/view.do")
 	public String rideHandle() {
