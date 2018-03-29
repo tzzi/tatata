e1f70ba8d3cff55d47030b90bc29df92e1fc08ed
1104-22 <1104-22@1104-22-PC>
2018-03-29 14:57:04 +0900

    2018-03-29 귀섭

--- src/beans/RideboardDAO.java
+++ src/beans/RideboardDAO.java
@@ -1,5 +1,6 @@
 package beans;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -12,73 +13,158 @@ import org.springframework.stereotype.Repository;
 public class RideboardDAO {
 
 	@Autowired
-	SqlSessionFactory factory;
+	   SqlSessionFactory factory;
 
-	// 게시물 등록
-	public int write(Map param) {
+	   // 게시물 등록
+	   public int write(Map param) {
+	      SqlSession session = factory.openSession();
+	      int rst = 0;
+	      try {
+	         System.out.println(param);
+	         rst = session.insert("rideboard.ride_write", param);
+	      } finally {
+	         session.close();
+	      }
+	      return rst;
+	   }
+
+	   // 전체 리스트 목록
+	   public List<Map> list() {
+	      SqlSession session = factory.openSession();
+	      try {
+	         return session.selectList("rideboard.list");
+	      } finally {
+	         session.close();
+	      }
+	   }
+
+	    //조회수 //게시판 상세보기
+	   public Map detail(String ride_name) {
+	      SqlSession session = factory.openSession();
+	      try {
+
+	         return session.selectOne("rideboard.detail", ride_name);
+
+	      } finally {
+	         session.close();
+	      }
+	   }
+
+	   // 조회수
+	   public int count(String ride_name) {
+	      SqlSession session = factory.openSession();
+	      try {
+	         int rst = 0;
+	         rst = session.update("rideboard.count", ride_name);
+	         return rst;
+	      } finally {
+	         session.close();
+	      }
+	   }
+
+	   // 공원 별
+	   public List<Map> parkdetail(String park_name) {
+	      SqlSession session = factory.openSession();
+	      try {
+	         System.out.println(park_name);
+
+	         return session.selectList("rideboard.parkdetail", park_name);
+	      } finally {
+	         session.close();
+	      }
+	   }
+
+	   // 좋아요 수
+	   public int goodcnt(String ride_name) {
+	      SqlSession session = factory.openSession();
+	      try {
+	         int rst = 0;
+	         rst = session.update("rideboard.goodcnt", ride_name);
+	         return rst;
+	      } finally {
+	         session.close();
+	      }
+	   }
+	// 장바구니
+	public int basket(Map map, String nick, String basket, String type) {
 		SqlSession session = factory.openSession();
 		int rst = 0;
 		try {
-			System.out.println(param);
-			rst = session.insert("rideboard.ride_write", param);
+			Map mylist = session.selectOne("basket.myinfo", nick);
+			System.out.println(mylist);
+			System.out.println(map);
+			System.out.println(nick);
+			System.out.println(basket);
+
+			Map map2 = new LinkedHashMap<>();
+			map2.put("basket", basket);
+			map2.put("type", type);
+
+			System.out.println(map2);
+
+			System.out.println(mylist);
+
+			if (mylist == null) {
+				System.out.println("닉네임으로된 정보가 없음. 새로운 장바구니 insert");
+				rst = session.update("basket.first", map);
+				return rst;
+			} else {
+				for (int a = 1; a<= 5; a++) {
+					if (mylist.get("BASKET" + a) != null) {
+						if (mylist.get("BASKET" + a).equals(basket)) {
+							return 5;// null이 아니고 같은 이름이 있다.
+						} else {// 널은 아니지만 같은 이름이 아니다.
+							System.out.println("비어있진 않지만 같은 놀이기구가 아니다.");
+						}
+					}else if (mylist.get("BASKET" + a) == null) {// 비어있다.
+						for(int b=1;b<=5;b++) {
+							
+						}
+						
+						System.out.println(a + "번 들어간다");
+						System.out.println(map);
+						System.out.println("basket.basket" + a);
+						rst = session.update("basket.basket" + a, map);
+						return rst;
+					}
+				}
+			}
+
+			// =======================================================
+			// 중복 데이터가 존재하지 않을 때
+			System.out.println("중복데이터 색출 완료");
+			if (mylist.get("BASKET1") == null) {
+				System.out.println("바스킷 1번 들어감 : " + rst);
+				rst = session.update("basket.basket1", map);
+				return rst;
+			} else if (mylist.get("BASKET2") == null) {
+				System.out.println("바스킷 2번 들어감 : " + rst);
+				rst = session.update("basket.basket2", map);
+				return rst;
+			} else if (mylist.get("BASKET3") == null) {
+				System.out.println("바스킷 3번 들어감 : " + rst);
+				rst = session.update("basket.basket3", map);
+				return rst;
+			} else if (mylist.get("BASKET4") == null) {
+				System.out.println("바스킷 4번 들어감 : " + rst);
+				rst = session.update("basket.basket4", map);
+				return rst;
+			} else if (mylist.get("BASKET5") == null) {
+				System.out.println("바스킷 5번 들어감 : " + rst);
+				rst = session.update("basket.basket5", map);
+				return rst;
+			} else {
+				rst = 3;
+				return rst;
+			}
+
+		} catch (Exception e) {
+			System.out.println("이셉션!");
+			e.printStackTrace();
 		} finally {
 			session.close();
 		}
 		return rst;
 	}
 
-	// 전체 리스트 목록
-	public List<Map> list() {
-		SqlSession session = factory.openSession();
-		try {
-			return session.selectList("rideboard.list");
-		} finally {
-			session.close();
-		}
-	}
-
-	// 좋아요 //조회수 //게시판 상세보기
-	public Map detail(String ride_name) {
-		SqlSession session = factory.openSession();
-		try {
-
-			return session.selectOne("rideboard.detail", ride_name);
-
-		} finally {
-			session.close();
-		}
-	}
-//좋아요 수
-	public int goodcnt(String ride_name) {
-		SqlSession session = factory.openSession();
-		try {
-			int rst = 0;
-			rst = session.update("rideboard.goodcnt", ride_name);
-			return rst;
-		} finally {
-			session.close();
-		}
-	}
-	// 조회수
-	public int count(String ride_name) {
-		SqlSession session = factory.openSession();
-		try {
-			int rst = 0;
-			rst = session.update("rideboard.count", ride_name);
-			return rst;
-		} finally {
-			session.close();
-		}
-	}
-	// 공원 별
-	public List<Map> parkdetail(String park_name) {
-		SqlSession session = factory.openSession();
-		try {
-			System.out.println(park_name);
-
-			return session.selectList("rideboard.parkdetail", park_name);
-		} finally {
-			session.close();
-		}
-	}
 }
--- src/controllers/RideController.java
+++ src/controllers/RideController.java
@@ -5,13 +5,17 @@ import java.sql.Date;
 import java.util.Map;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
 import beans.RideboardDAO;
@@ -19,87 +23,124 @@ import beans.RideboardDAO;
 @Controller
 @RequestMapping("/rideboard")
 public class RideController {
-	@Autowired
-	ServletContext ctx;
+   @Autowired
+   ServletContext ctx;
 
-	@Autowired
-	RideboardDAO rdao;
+   @Autowired
+   RideboardDAO rdao;
 
-	@RequestMapping("/index.do")
-	public String ride_indexHandle() {
+   @RequestMapping("/index.do")
+   public String ride_indexHandle() {
 
-		return "ride_index";
+      return "ride_index";
 
-	}
+   }
 
-	@RequestMapping("/write.do")
-	public String writeHandle() {
-		return "write";
-	}
+   @RequestMapping("/write.do")
+   public String writeHandle() {
+      return "write";
+   }
 
-	@RequestMapping("/complete.do")
-	public String ride_writeHandle(@RequestParam Map<String, String> param, ModelMap map,MultipartFile file, HttpServletRequest req) {
-		String park_name = param.get("park_name");
-		String ride_name = param.get("ride_name");
-		String type = param.get("type");
-		String content = param.get("content");
-		String image = param.get("image");
-		String url = "/image/";
-		Date left_date = new Date(System.currentTimeMillis());
-		
-		File uploaddir = new File(ctx.getRealPath("/image"));
-		uploaddir.mkdirs();
-		String fileName = System.currentTimeMillis() + ".jpg";
-		File dst = new File(uploaddir, fileName);
-		try {
-			file.transferTo(dst);
-		} catch (Exception e) {
-			e.printStackTrace();
+   @RequestMapping("/complete.do")
+   public String ride_writeHandle(@RequestParam Map<String, String> param, ModelMap map,MultipartFile file, HttpServletRequest req) {
+      String park_name = param.get("park_name");
+      String ride_name = param.get("ride_name");
+      String type = param.get("type");
+      String content = param.get("content");
+      String image = param.get("image");
+      String url = "/image/";
+      Date left_date = new Date(System.currentTimeMillis());
+      
+      File uploaddir = new File(ctx.getRealPath("/image"));
+      uploaddir.mkdirs();
+      String fileName = System.currentTimeMillis() + ".jpg";
+      File dst = new File(uploaddir, fileName);
+      try {
+         file.transferTo(dst);
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+      map.put("park_name", park_name);
+      map.put("ride_name", ride_name);
+      map.put("type", type);
+      map.put("content", content);
+      map.put("image",url+dst.getName());
+      map.put("left_date", left_date);
+      
+      System.out.println(map);
+      
+      req.setAttribute("park_name", park_name);
+      req.setAttribute("ride_name", ride_name);
+      req.setAttribute("type", type);
+      req.setAttribute("image", image );
+      req.setAttribute("content", content);
+      req.setAttribute("left_date", left_date);
+      int rst = rdao.write(map);
+      if (rst == 1) {
+         return "write_complete";
+      } else {
+         return "write";
+      }
+
+   }
+   //놀이기구 전체 목록
+   @RequestMapping("/list.do")
+   public String listHandle(ModelMap modelMap) {
+      System.out.println(rdao.list());
+      modelMap.put("rideboard",rdao.list());
+      return "list";
+   }
+   // 좋아요 수 증가
+   @RequestMapping(path = "/ride_like.do", produces = "application/json;charset=utf-8")
+   @ResponseBody
+   public String addlikeController(@RequestParam String ride_name) {
+      int rst = rdao.goodcnt(ride_name);
+      
+      String b = "0";
+      if(rst==1) {
+         b = "1";
+      }else {
+         b = "2";
+      }
+      return "[{\"result\":" + b + "}]";
+            
+   }
+   //조회수 , 상세보기
+   @RequestMapping("/detail.do")
+   public String detailHandle(@RequestParam String ride_name,ModelMap modelMap){
+      
+      modelMap.put("rideboard", rdao.count(ride_name));
+      modelMap.put("rideboard",rdao.detail(ride_name));
+      System.out.println(modelMap);
+      
+      
+      return "detail";
+   }
+   
+   @RequestMapping("/parkdetail.do")
+   public String parkdetailHandle(@RequestParam String park_name, ModelMap modelMap) {
+   
+      modelMap.put("rideboard", rdao.parkdetail(park_name));
+      return "parkdetail";
+   }
+   @RequestMapping(path = "/cart.do", produces = "application/json;charset=utf-8")
+	@ResponseBody
+	public String basketHandle(@RequestParam Map map,@RequestParam String nick,@RequestParam String basket,@RequestParam String type) {
+		String a=null;
+		int rst= 0;
+		rst = rdao.basket(map, nick, basket, type);		
+		System.out.println("rst값 : " + rst);
+		if(rst==5) {
+		 a ="5";
+		}else if(rst==1) {
+			a = "1";
+		}else if(rst==3) {
+			a = "3";
 		}
-		map.put("park_name", park_name);
-		map.put("ride_name", ride_name);
-		map.put("type", type);
-		map.put("content", content);
-		map.put("image",url+dst.getName());
-		map.put("left_date", left_date);
-		
-		System.out.println(map);
-		
-		req.setAttribute("park_name", park_name);
-		req.setAttribute("ride_name", ride_name);
-		req.setAttribute("type", type);
-		req.setAttribute("image", image );
-		req.setAttribute("content", content);
-		req.setAttribute("left_date", left_date);
-		int rst = rdao.write(map);
-		if (rst == 1) {
-			return "write_complete";
-		} else {
-			return "write";
-		}
 
+		return "[{\"result\":" + a + "}]";
+		
 	}
-	@RequestMapping("/list.do")
-	public String listHandle(ModelMap modelMap) {
-		System.out.println(rdao.list());
-		modelMap.put("rideboard",rdao.list());
-		return "list";
-	}
-	//좋아요 , 조회수 , 상세보기
-	@RequestMapping("/detail.do")
-	public String detailHandle(@RequestParam String ride_name,ModelMap modelMap) {
-		modelMap.put("rideboard", rdao.goodcnt(ride_name));
-		modelMap.put("rideboard", rdao.count(ride_name));
-		modelMap.put("rideboard",rdao.detail(ride_name));
-		System.out.println(modelMap);
-		return "detail";
-	}
-	
-	
-	@RequestMapping("/parkdetail.do")
-	public String parkdetailHandle(@RequestParam String park_name, ModelMap modelMap) {
-	
-		modelMap.put("rideboard", rdao.parkdetail(park_name));
-		return "parkdetail";
-	}
+
+   
 }
